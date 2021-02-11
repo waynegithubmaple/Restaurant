9fe4c53bcefe7add053d38d7430f9db4dcfc690a
Wayne <lowbw-ab13@student.tarc.edu.my>
2021-02-11 20:28:33 +0800

    I have added and redesigned "Register Food"

--- TarucRestaurant/FoodSettings.vb
+++
@@ -1,3 +0,0 @@
-﻿Public Class FoodSettings
-
-End Class
--- TarucRestaurant/My Project/Application.Designer.vb
+++ TarucRestaurant/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.TarucRestaurant.FoodSettings
+            Me.MainForm = Global.TarucRestaurant.frmCreateFood
         End Sub
     End Class
 End Namespace
