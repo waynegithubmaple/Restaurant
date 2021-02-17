9cfef9c69ace15a315137cb58066de40d625ad0b
Lim Cia Ler <ler1997.lcl@gmail.com>
2021-02-17 23:04:26 +0800

    Add a manager CRU form that only has a 'Reservation' function.

---
+++ VP Assignment 2/frmManagerCRU.Designer.vb
@@ -0,0 +1,79 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmManagerCRU
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnReservation = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(98, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(191, 31)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Welcome back"
+        '
+        'btnReservation
+        '
+        Me.btnReservation.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnReservation.Location = New System.Drawing.Point(135, 195)
+        Me.btnReservation.Name = "btnReservation"
+        Me.btnReservation.Size = New System.Drawing.Size(121, 29)
+        Me.btnReservation.TabIndex = 9
+        Me.btnReservation.Text = "&Reservation"
+        Me.btnReservation.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Times New Roman", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Blue
+        Me.Label2.Location = New System.Drawing.Point(74, 51)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(243, 40)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "XXX Manager"
+        '
+        'frmManagerCRU
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(384, 449)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnReservation)
+        Me.Name = "frmManagerCRU"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "frmManagerCRU"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnReservation As Button
+    Friend WithEvents Label2 As Label
+End Class
---
+++ VP Assignment 2/frmManagerCRU.vb
@@ -0,0 +1,19 @@
+﻿Public Class frmManagerCRU
+
+    Public Property name666 As String
+
+
+    Private Sub btnOrder_Click(sender As Object, e As EventArgs)
+        frmMainOrder.Show()
+    End Sub
+
+    Private Sub btnReservation_Click(sender As Object, e As EventArgs) Handles btnReservation.Click
+        'Form2.Show()
+    End Sub
+
+    Private Sub btnPayment_Click(sender As Object, e As EventArgs)
+        frmOder.Show()
+    End Sub
+
+
+End Class
