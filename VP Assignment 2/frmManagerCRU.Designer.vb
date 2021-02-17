2f28b803acc49f0f0d58af904961c92346da06c5
Lim Cia Ler <ler1997.lcl@gmail.com>
2021-02-17 23:47:32 +0800

    Add a 'Close' button at frmManagerCRU

--- VP Assignment 2/frmManagerCRU.Designer.vb
+++ VP Assignment 2/frmManagerCRU.Designer.vb
@@ -25,6 +25,7 @@ Partial Class frmManagerCRU
         Me.Label1 = New System.Windows.Forms.Label()
         Me.btnReservation = New System.Windows.Forms.Button()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
@@ -58,11 +59,22 @@ Partial Class frmManagerCRU
         Me.Label2.TabIndex = 11
         Me.Label2.Text = "XXX Manager"
         '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(135, 351)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(121, 29)
+        Me.btnExit.TabIndex = 12
+        Me.btnExit.Text = "&Close"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
         'frmManagerCRU
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(384, 449)
+        Me.Controls.Add(Me.btnExit)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.btnReservation)
@@ -76,4 +88,5 @@ Partial Class frmManagerCRU
     Friend WithEvents Label1 As Label
     Friend WithEvents btnReservation As Button
     Friend WithEvents Label2 As Label
+    Friend WithEvents btnExit As Button
 End Class
--- VP Assignment 2/frmManagerCRU.vb
+++ VP Assignment 2/frmManagerCRU.vb
@@ -15,5 +15,7 @@
         frmOder.Show()
     End Sub
 
-
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Application.Exit()
+    End Sub
 End Class
