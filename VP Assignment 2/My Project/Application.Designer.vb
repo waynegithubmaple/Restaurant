9fe48951e550c1f027c0c13ee11a339e56c67700
Dania Rachid <rachidd-wp18@student.tarc.edu.my>
2021-03-01 14:42:47 +0300

    Revert "add viewOrder and UpdateOrder functions"
    This reverts commit 2fa9c6ba007176b647165a991aec1de80ca89266.

--- VP Assignment 2/My Project/Application.Designer.vb
+++ VP Assignment 2/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.VP_Assignment_2.frmMainOrder
+            Me.MainForm = Global.VP_Assignment_2.FrmLogin
         End Sub
     End Class
 End Namespace
--- VP Assignment 2/frmMainOrder.vb
+++ VP Assignment 2/frmMainOrder.vb
@@ -20,20 +20,4 @@
     Private Sub btnAddOrder_Click(sender As Object, e As EventArgs) Handles btnAddOrder.Click
         frmAvailableTables.Show()
     End Sub
-
-    Private Sub btnViewOrder_Click(sender As Object, e As EventArgs) Handles btnViewOrder.Click
-        Dim db As New restaurantDataContext
-        Dim s = (From o In db.Seats Where o.TableStatus = "Unavailable").ToArray
-        If s.Length > 0 Then
-            frmViewOrder.Show()
-        Else
-            MessageBox.Show("There are no orders", "information", MessageBoxButtons.OK, MessageBoxIcon.Information)
-
-        End If
-    End Sub
-
-    Private Sub btnUpdateOrder_Click(sender As Object, e As EventArgs) Handles btnUpdateOrder.Click
-        selectedChoice = "Update"
-        frmUnavailableTables.Show()
-    End Sub
 End Class
